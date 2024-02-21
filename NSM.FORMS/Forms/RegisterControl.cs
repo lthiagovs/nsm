9e69a1e5d2a7811018aafda71c56389294f4cde6
Luis Thiago Vasconcelos de Farias <lthiagofarias@hotmail.com>
2024-02-21 20:15:04 -0300

    Tela de registro mais segura

--- NSM.FORMS/Forms/RegisterControl.cs
+++ NSM.FORMS/Forms/RegisterControl.cs
@@ -7,17 +7,24 @@ namespace NSM.FORMS.Forms
 {
     public partial class RegisterControl : UserControl
     {
-        public bool ValidarRegistro(string login, string senha)
+        public bool ValidarRegistro(string login, string senha, string senha2, string nome)
         {
             if (login.Length < 3) { MessageBox.Show("O nome de usuário deve ter pelo menos 3 caracteres", "Erro",
                 MessageBoxButtons.OK, MessageBoxIcon.Information); return false; }
             if (!Regex.IsMatch(login, "^[a-zA-Z0-9_.]+$")) { MessageBox.Show("O nome de usuário deve conter apenas letras, números, '.', ou '_'.",
                 "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information); return false; }
-            if (senha.Length < 8 || !Regex.IsMatch(senha, @"^(?=.*[a-zA-Z])(?=.*\d).+$"))
+            if (senha.Length < 8 || senha2.Length < 8 || !Regex.IsMatch(senha, @"^(?=.*[a-zA-Z])(?=.*\d).+$"))
             {
                 MessageBox.Show("A senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra e um número.", "Erro",
                 MessageBoxButtons.OK, MessageBoxIcon.Information); return false;
             }
+            if (nome.Length <= 5)
+            {
+                MessageBox.Show("O nome de usuario deve possuir mais de 5 caracteres.", "Erro");
+                return false;
+            }
+
+
             return true;
         }
         public RegisterControl()
@@ -27,7 +34,7 @@ namespace NSM.FORMS.Forms
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            if (ValidarRegistro(txtLogin.Text, txtPassword.Text))
+            if (ValidarRegistro(txtLogin.Text, txtPassword.Text, txtPasswordRepeat.Text,txtName.Text))
             {
                 MessagePackage MessagePackage = new MessagePackage();
                 MessagePackage.MessageType = MessageType.Message_CreateUser;
