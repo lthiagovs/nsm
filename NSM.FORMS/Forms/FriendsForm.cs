aebe281ffc73d4ec91d600de18107d021628db21
Luis Thiago Vasconcelos de Farias <lthiagofarias@hotmail.com>
2024-02-27 13:59:11 -0300

    Merge

--- NSM.FORMS/Forms/FriendsForm.cs
+++ NSM.FORMS/Forms/FriendsForm.cs
@@ -1,24 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace NSM.FORMS.Forms
 {
-<<<<<<<< HEAD:NSM.FORMS/Forms/LoginForm.cs
-    public partial class LoginForm : Form
-    {
-        public LoginForm()
-========
-    public partial class FriendsForm : Form
-    {
+public partial class FriendsForm : Form
+{
         public FriendsForm()
->>>>>>>> Thiago_branch:NSM.FORMS/Forms/FriendsForm.cs
         {
             InitializeComponent();
         }
